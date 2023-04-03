66bcb1e19cc56115740a5bde80de180f79223c0e
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-04 00:05:56 +0900

    Level - Bronze 5

---
+++ Step-by-Step/Iteration Statements/no_2438.java
@@ -0,0 +1,36 @@
+package BaekJoon;
+
+/*문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+예제 입력 1 
+5
+예제 출력 1 
+*
+**
+***
+****
+******/
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i+1; j++) {
+				System.out.print("*");
+			}System.out.println();
+		}sc.close();
+	}
+}
+
+
