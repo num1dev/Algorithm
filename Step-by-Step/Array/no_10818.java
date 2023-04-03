a52cb386e168a031278fe000253302fd7beb131a
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-04 00:44:14 +0900

    Level - Bronze 3

---
+++ Step-by-Step/Array/no_10818.java
@@ -0,0 +1,44 @@
+package BaekJoon;
+
+/*문제
+N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+출력
+첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+예제 입력 1 
+5
+20 10 35 30 7
+예제 출력 1 
+7 35*/
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		int max = 0;
+		int min = 0;
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		 max = arr[0];
+	        min = arr[0];
+	        for (int i = 1; i < N; i++) {
+	            if (arr[i] > max) {
+	                max = arr[i];
+	            }
+	            if (arr[i] < min) {
+	                min = arr[i];
+	            }
+	        }
+	       
+	        System.out.println(min + " " + max);
+	    }
+	}
