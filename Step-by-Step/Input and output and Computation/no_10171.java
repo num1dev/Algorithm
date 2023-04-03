fd04bd761c516d7d73d1d76ff7e8d8229cf2fe5c
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-03 12:30:38 +0900

    no_10171

---
+++ Step-by-Step/Input and output and Computation/no_10171.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+/*문제
+아래 예제와 같이 고양이를 출력하시오.
+
+입력
+없음.
+
+출력
+고양이를 출력한다.
+
+예제 입력 1 
+예제 출력 1 
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|*/
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
+	
