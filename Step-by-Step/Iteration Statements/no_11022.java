dc3ff4b4be82f04e1724f7f93e3b5f418238b655
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-04 00:04:22 +0900

    Level - Bronze 5

---
+++ Step-by-Step/Iteration Statements/no_11022.java
@@ -0,0 +1,50 @@
+package BaekJoon;
+
+/*문제
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+
+예제 입력 1 
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+예제 출력 1 
+Case #1: 1 + 1 = 2
+Case #2: 2 + 3 = 5
+Case #3: 3 + 4 = 7
+Case #4: 9 + 8 = 17
+Case #5: 5 + 2 = 7*/
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int A[] = new int[T];
+		int B[] = new int[T];
+		int sum[] = new int[T];
+		for (int i = 0; i < T; i++) {
+			A[i] = sc.nextInt();
+			B[i] = sc.nextInt();
+			sum[i] = A[i] + B[i];
+		}
+		for (int j = 0; j < T; j++) {
+			System.out.printf( "Case #%d: %d + %d = %d\n", j+1, A[j], B[j], sum[j]);
+		}
+		sc.close();
+	}
+}
+
