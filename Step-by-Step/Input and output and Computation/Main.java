7e59b2037c18ee88278fb99108324289034da5a9
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-03 12:04:40 +0900

    no.1000

--- Step-by-Step/Input and output and Computation/Main.java
+++ Step-by-Step/Input and output and Computation/Main.java
@@ -1,16 +1,21 @@
 package baekjoon;
 
 /*문제
-Hello World!를 출력하시오.
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
 입력
-없음
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
 출력
-Hello World!를 출력하시오.*/
+첫째 줄에 A+B를 출력한다.*/
 
+import java.util.Scanner;
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    sc.close();
+	    System.out.println(a + b);
 	}
 }
